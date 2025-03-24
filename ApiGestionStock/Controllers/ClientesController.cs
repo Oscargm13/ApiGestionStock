b14bb71af4b28d05fc9e535774d86e5925126c70
Oscargm13 <oskitargm5@gmail.com>
2025-03-24 20:32:37 +0100

    Add project files.

---
+++ ApiGestionStock/Controllers/ClientesController.cs
@@ -0,0 +1,53 @@
+﻿using ApiGestionStock.Models;
+using ApiGestionStock.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiGestionStock.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientesController : ControllerBase
+    {
+        private RepositoryClientes repo;
+        public ClientesController(RepositoryClientes repo)
+        {
+            this.repo = repo;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Cliente>>>GetDepartamentos()
+        {
+            return await this.repo.GetClientesAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Cliente>> FindDepartamento(int id)
+        {
+            return await this.repo.FindClienteAsync(id);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> InsertDepartamento(Cliente cliente)
+        {
+            await this.repo.CreateClienteAsync(cliente.IdCliente, cliente.Nombre, cliente.Apellido, cliente.Email, cliente.Direccion,
+                cliente.Telefono, cliente.FechaNacimiento, cliente.Genero);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteDepartamento(int id)
+        {
+            await this.repo.DeleteClienteAsync(id);
+            return Ok();
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateDepartamento(Cliente cliente)
+        {
+            await this.repo.UpdateClienteAsync(cliente.IdCliente, cliente.Nombre, cliente.Apellido, cliente.Email, cliente.Direccion,
+                cliente.Telefono, cliente.FechaNacimiento, cliente.Genero);
+            return Ok();
+        }
+    }
+}
---
+++ ApiGestionStock/Data/AlmacenesContext.cs
@@ -0,0 +1,11 @@
+﻿using ApiGestionStock.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiGestionStock.Data
+{
+    public class AlmacenesContext: DbContext
+    {
+        public AlmacenesContext(DbContextOptions<AlmacenesContext> options): base(options) { }
+        public DbSet<Cliente> Clientes { get; set; }
+    }
+}
